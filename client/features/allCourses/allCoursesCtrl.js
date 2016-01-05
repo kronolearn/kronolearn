ea8a0b9ba98231f10134e857c7cd02f7bcb271b7
Kyle <kyle.lauritzen@gmail.com>
2016-01-05 16:28:20 -0700

    added scss file for allCourses, working on allCourses feature

--- client/features/allCourses/allCoursesCtrl.js
+++ client/features/allCourses/allCoursesCtrl.js
@@ -3,4 +3,8 @@ var app = angular.module('kronolearn');
 
 app.controller('allCoursesCtrl', function($scope) {
     
+    $scope.topicsShowing = false;
+    
+    $scope.topicsShowing2 = false;
+    
 });
