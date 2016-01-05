a1945260681badf8da78c317e2347588fb80533e
SGMartineau <winteralce@gmail.com>
2016-01-05 11:51:58 -0700

    set up the app.js routes

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,40 @@
-var app = angular.module('kronolearn', [ui.router]);
+var app = angular.module('kronolearn', ['ui.router']);
 
 
-app.config(function($stateProvider, $urlProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
     
+    $stateProvider
+    
+    .state('home', {
+        url: '/',
+        templateUrl: './features/home/homeTmpl.html',
+        controller: 'homeCtrl'
+    })
+    
+    .state('topic', {
+        url: '/topic/:topicId',
+        templateUrl: './features/topic/topicTmpl.html',
+        controller: 'topicCtrl'
+    })
+    
+    .state('dashboard', {
+        url: '/dashboard/:dashboardId',
+        templateUrl: './features/dashboard/dashboardTmpl.html',
+        controller: 'dashboardCtrl'
+    })
+    
+    .state('course', {
+        url: '/course/:courseId',
+        templateUrl: './features/course/courseTmpl.html',
+        controller: 'courseCtrl'
+    })
+    
+    .state('allCourses', {
+        url: '/allCourses',
+        templateUrl: './features/allCourses/allCoursesTmpl.html',
+        controller: 'allCoursesCtrl'
+    });
+    
+    $urlRouterProvider.otherwise('/');
     
 });
