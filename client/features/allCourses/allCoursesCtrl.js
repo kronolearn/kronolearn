9358b7ba49adc5845b3e9c320f0780307e6c17e3
Kyle <kyle.lauritzen@gmail.com>
2016-01-06 13:55:24 -0700

    limited typeahead to only search for name property

--- client/features/allCourses/allCoursesCtrl.js
+++ client/features/allCourses/allCoursesCtrl.js
@@ -10,33 +10,36 @@ app.controller('allCoursesCtrl', function($scope) {
     $scope.allCoursesArray = [
         
         {
-            name: "Javascript",
             description: "An intro course to the Javascript language",
             subjects: ["Computers", "Programming", "Web"],
             topics: ["Arrays", "Functions", "Closures"],
             admins: "Kyle",
             students: [83049, 20493],
-            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/logo_JavaScript.png"
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/logo_JavaScript.png",
+            name: "Javascript"
+
         },
         
         {
-            name: "React",
             description: "Learn the basics of React!",
             subjects: ["Computers", "Programming", "Web"],
             topics: ["Components", "Webpack", "View"],
             admins: "Kyle",
             students: [83049, 20493],
-            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/addthis-react-flux-javascript-scaling.png"
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/addthis-react-flux-javascript-scaling.png",
+            name: "React"
+
         },
         
         {
-            name: "HTML",
             description: "Learn the fundamentals of HTML with this HTML basics course!",
             subjects: ["Computers", "Programming", "Web"],
             topics: ["Elements", "Divs", "<p>"],
             admins: "Kyle",
             students: [83049, 20493],
-            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/htmlimage.jpeg"
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/htmlimage.jpeg",
+            name: "HTML"
+
         }
     ];
     
