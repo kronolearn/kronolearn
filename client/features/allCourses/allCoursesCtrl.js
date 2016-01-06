155bfe2b4d7e20b2f949b2b15db28d8e01ce7d9b
Kyle <kyle.lauritzen@gmail.com>
2016-01-06 10:51:55 -0700

    adjusted the view of allCourses, adding image and description

--- client/features/allCourses/allCoursesCtrl.js
+++ client/features/allCourses/allCoursesCtrl.js
@@ -19,12 +19,22 @@ app.controller('allCoursesCtrl', function($scope) {
         
         {
             name: "React",
-            description: "An intro course to the Javascript language",
+            description: "Learn the basics of React!",
             subjects: ["Computers", "Programming", "Web"],
             topics: ["Components", "Webpack", "View"],
             admins: "Kyle",
             students: [83049, 20493],
             picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/addthis-react-flux-javascript-scaling.png"
+        },
+        
+        {
+            name: "HTML",
+            description: "Learn the fundamentals of HTML with this HTML basics course!",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Elements", "Divs", "<p>"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/htmlimage.jpeg"
         }
     ];
     
