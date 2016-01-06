1a7eb296b15184bc9c9ac10eba180ca5893cde34
Kyle <kyle.lauritzen@gmail.com>
2016-01-06 10:18:49 -0700

    courses togglable, searchable by course name

--- client/features/allCourses/allCoursesCtrl.js
+++ client/features/allCourses/allCoursesCtrl.js
@@ -4,7 +4,28 @@ var app = angular.module('kronolearn');
 app.controller('allCoursesCtrl', function($scope) {
     
     $scope.topicsShowing = false;
-    
-    $scope.topicsShowing2 = false;
+        
+    $scope.allCoursesArray = [
+        
+        {
+            name: "Javascript",
+            description: "An intro course to the Javascript language",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Arrays", "Functions", "Closures"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/logo_JavaScript.png"
+        },
+        
+        {
+            name: "React",
+            description: "An intro course to the Javascript language",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Components", "Webpack", "View"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/addthis-react-flux-javascript-scaling.png"
+        }
+    ];
     
 });
