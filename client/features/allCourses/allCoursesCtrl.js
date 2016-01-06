c7e8d2093b5978ba3ca81d420a1047b105264f50
Kyle <kyle.lauritzen@gmail.com>
2016-01-06 12:25:58 -0700

    adding new files?

--- client/features/allCourses/allCoursesCtrl.js
+++ client/features/allCourses/allCoursesCtrl.js
@@ -3,4 +3,41 @@ var app = angular.module('kronolearn');
 
 app.controller('allCoursesCtrl', function($scope) {
     
+    $scope.topicsShowing = false;
+    
+    $scope.coursesearch = undefined;
+        
+    $scope.allCoursesArray = [
+        
+        {
+            name: "Javascript",
+            description: "An intro course to the Javascript language",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Arrays", "Functions", "Closures"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/logo_JavaScript.png"
+        },
+        
+        {
+            name: "React",
+            description: "Learn the basics of React!",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Components", "Webpack", "View"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/addthis-react-flux-javascript-scaling.png"
+        },
+        
+        {
+            name: "HTML",
+            description: "Learn the fundamentals of HTML with this HTML basics course!",
+            subjects: ["Computers", "Programming", "Web"],
+            topics: ["Elements", "Divs", "<p>"],
+            admins: "Kyle",
+            students: [83049, 20493],
+            picture: "https://s3-us-west-2.amazonaws.com/kronolearn/coursePics/htmlimage.jpeg"
+        }
+    ];
+    
 });
