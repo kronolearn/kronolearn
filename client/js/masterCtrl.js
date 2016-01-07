0c06c142d064979c2ed59f4712462444d3b49a77
Kyle <kyle.lauritzen@gmail.com>
2016-01-07 10:15:47 -0700

    added typeAhead functionality to nav search bar!

--- client/js/masterCtrl.js
+++ client/js/masterCtrl.js
@@ -3,5 +3,20 @@ var app = angular.module('kronolearn');
 
 app.controller('masterCtrl', function($scope, masterService) {
     
+        $scope.mastercoursesearch = undefined;
+    
+        $scope.masterCoursesArray = [];
+    
+        $scope.getAllCourses = function() {
+        masterService.getAllCourses().then(function(response) {
+          //  console.log("This is back-end MASTERCtrl response", response);
+            for (var i = 0; i < response.length; i++) {
+                $scope.masterCoursesArray.push(response[i]);
+            }
+        });
+    };
+    
+        //runs every time masterCtrl loads, in order to have constant nav-bar typeahead capability
+        $scope.getAllCourses();
     
 });
