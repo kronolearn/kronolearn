b7623a14692ba19bd29f870ac3d30b101cda9a57
Kellie Green <kelliegreen10690@gmail.com>
2016-01-06 22:08:25 -0700

    fixed the gray fade on dashboard after logging in, also fixed login from keeping user info after logout

--- client/features/dashboard/dashboardCtrl.js
+++ client/features/dashboard/dashboardCtrl.js
@@ -3,9 +3,5 @@ var app = angular.module('kronolearn');
 
 app.controller('dashboardCtrl', function($scope, dashboardService) {
     
-        // console.log($scope.user);
-    $scope.out = function() {
-        dashboardService.logout(); 
-    };
     
 });
--- server/models/Topic.js
+++ server/models/Topic.js
@@ -1,15 +1,22 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
+    
+// var tags = ['tag1', 'tag2'];
+
+// var tags = 
+
+
 	
 var	TopicSchema = Schema ({
 	name: { type: String },
-	tags: [{ type: String }],
+	tags: [{ type: String, default: 'tag' }],
 	description: { type: String },
 	picture: { type: String },
 	reviewMaterials: [{
 	    material: { type: String },
-        reviewTags: [{ type: Schema.Types.ObjectId, ref: 'Topic' }]
-        // reviewTags: [{ type: String, enum: TopicSchema.tags }], this line gives this error. "reviewTags: [{ type: String, enum: TopicSchema.tags }], TypeError: Cannot read property 'tags' of undefined" We are going to use Ref until Peter gets back tomorrow!
+        // reviewTags: [{ type: Schema.Types.ObjectId, ref: 'Topic' }]
+        reviewTags: [{ type: String }]
+        //, this line gives this error. "reviewTags: [{ type: String, enum: TopicSchema.tags }], TypeError: Cannot read property 'tags' of undefined" We are going to use Ref until Peter gets back tomorrow!
                                                       
 		// Array of Strings, enum to tags of topic (dropdown)
 }],
