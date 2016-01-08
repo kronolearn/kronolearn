5df3e939f64ea6b8c103bcbeee3dafa47bffff6d
Peter Krieg <pkrieg2@gmail.com>
2016-01-08 14:57:17 -0700

    continued course layout, made clicking topic go to topic state.  change app.js to have state topic include courseId and topicId in state params

--- client/features/topic/topicCtrl.js
+++ client/features/topic/topicCtrl.js
@@ -1,14 +1,25 @@
 var app = angular.module("kronolearn");
 
-app.controller('topicCtrl', function($scope, topicService, $stateParams) {
+app.controller('topicCtrl', function($scope, topicService, $stateParams, courseService) {
+	var topicId = $stateParams.topicId;
+	var courseId = $stateParams.courseId;
+
+	$scope.getCourse = function(){
+		courseService.getCourse(courseId)
+		.then(function(course){
+			$scope.course = course;
+		})
+	}
+
     
     $scope.getTopic = function () {
-        topicService.getTopic($stateParams.topicId).then(function(response) {
+        topicService.getTopic(topicId).then(function(response) {
             $scope.topic = response;
             $scope.reviewMaterials = response.reviewMaterials;
         })
     }
     
+    $scope.getCourse();
     $scope.getTopic();
     
 });
