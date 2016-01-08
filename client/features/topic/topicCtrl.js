0db4f6f75c8906ee44d376439afefbde3798abac
SGMartineau <winteralce@gmail.com>
2016-01-08 15:01:48 -0700

    I put in some graphs on the topic page

--- client/features/topic/topicCtrl.js
+++ client/features/topic/topicCtrl.js
@@ -1,14 +1,37 @@
 var app = angular.module("kronolearn");
 
-app.controller('topicCtrl', function($scope, topicService, $stateParams) {
+app.controller('topicCtrl', function ($scope, topicService, $stateParams) {
+
     
+    function progress(percent, element) {
+        var progressBarWidth = percent * element.width() / 100;
+        // With labels:
+        element.find('div').animate({
+            width: progressBarWidth
+        }, 1200).html(percent + "%&nbsp;");
+
+        // Without labels:
+        //element.find('div').animate({ width: progressBarWidth }, 500);
+    }
+
+    $(document).ready(function () {
+        $('.progressBar').each(function () {
+            //alert('Hello');
+            var bar = $(this);
+            var max = $(this).attr('id');
+            
+
+            progress(max, bar);
+        });
+    });
+
     $scope.getTopic = function () {
-        topicService.getTopic($stateParams.topicId).then(function(response) {
+        topicService.getTopic($stateParams.topicId).then(function (response) {
             $scope.topic = response;
             $scope.reviewMaterials = response.reviewMaterials;
         })
     }
-    
+
     $scope.getTopic();
-    
+
 });
