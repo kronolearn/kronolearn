3ed29e56ce26663fa650d55b998cd1905b3783d4
Kellie Green <kelliegreen10690@gmail.com>
2016-01-11 11:39:34 -0700

    ng-repeat issues

--- client/features/dashboard/dashboardCtrl.js
+++ client/features/dashboard/dashboardCtrl.js
@@ -3,13 +3,45 @@ var app = angular.module('kronolearn');
 
 app.controller('dashboardCtrl', function($scope, dashboardService, user) {
 	// user is passed into dashboard ctrl from resolve in app.js
-  // console.log(user);
   $scope.user = user;
-
+  
+  $scope.list = [{name: 'hello'}, {name: 'blah'}];
+  
+  
+  $scope.courses = [{
+      name: "Our first course!",
+      description: "My Description, whoo!",
+      admins: ["kellie", "Josh"],
+      topics: ["topic1", "topic2"],
+      subjects : [ 
+        "subject one", 
+        "subject two"
+    ],
+      picture: "http://img.wikinut.com/img/gycf69_-6rv_5fol/jpeg/0/Best-Friends-Img-Src%3AImage%3A-FreeDigitalPhotos.net.jpeg"
+  }];
+  console.log($scope.courses);
+//   $scope.userInfo = dashboardService.getUserAndCourses();
+  
+//   console.log($scope.userInfo);
+//   console.log($scope.user);
+  
+  
+//   $scope.courses = $scope.user.coursesEnrolledIn;
+ 
+//   $scope.enrolledCourses = {};
+//   $scope.getCourses = function() {
+//       for(var i = 0; i < $scope.courses.length; i++) {
+//           $scope.enrolledCourses += $scope.courses[i]; 
+//           console.log($scope.courses[i]);
+//       }
+//   };
+  
+//   $scope.getCourses();
+//   console.log($scope.enrolledCourses);
 
   
 
-
+// console.log("Hello");
 
 
 
