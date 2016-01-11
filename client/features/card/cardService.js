51b49083590865b8f95ca17b54d38a9e55c9a823
Kyle <kyle.lauritzen@gmail.com>
2016-01-11 11:07:50 -0700

    working on Card view

--- client/features/card/cardService.js
+++ client/features/card/cardService.js
@@ -3,4 +3,10 @@ var app = angular.module('kronolearn');
 
 app.service('cardService', function($http) {
     
+    this.getCardInfo = function(topicId) {
+        return $http.get('/api/topic/' + topicId).then(function(response){
+            console.log("This is CARD response in cardService: ", response);
+            return response.data;
+        })
+    };
 });
