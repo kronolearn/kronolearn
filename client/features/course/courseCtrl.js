6d84a238c2537a0acc46a5e937baeaf8362be96b
Peter Krieg <pkrieg2@gmail.com>
2016-01-14 13:55:10 -0700

    changed course view so that only shows ability to add topic if user is admin of course

--- client/features/course/courseCtrl.js
+++ client/features/course/courseCtrl.js
@@ -1,61 +1,75 @@
 angular.module('kronolearn')
 .controller('courseCtrl', function($scope, userService, masterService, courseService, $stateParams, $state) {
 
-	var courseId = $stateParams.courseId;
-    
+	var courseNumber = $stateParams.courseId;
 
-	$scope.message = 'hello';
 
 	userService.checkUserLogin()
 	.then(function(user){
 		console.log(user);
 		$scope.user = user;
-	})
+		var userId = user._id;
 
-	// practice hooking up front end to angular, will make call to server eventually
+	   //runs every time this controller loads, in order to grab the currentCourse info from the back-end
+	   masterService.getCurrentCourse(courseNumber)
+	   .then(function(course) {
+	   	console.log("COURSE RESPONSE:", course);
+	   	$scope.course = course;
 
-    $scope.currentCourseNumber = $stateParams;
-    $scope.getCurrentCourse = function() {
-        console.log("===test 1: ", $scope.currentCourseNumber);
-        masterService.getCurrentCourse($scope.currentCourseNumber).then(function(response) {
-            
-            console.log("ULTIMATE RESPONSE:", response);
-            $scope.currentCourse = response;
-      });
-    }
-    
-    $scope.goToTopic = function (id) {
-        $state.go('topic', { topicId: id });
-    }
-                                                                
-    
-   //runs every time this controller loads, in order to grab the currentCourse info from the back-end
-    $scope.currentCourse = $scope.course;
-    $scope.getCurrentCourse();
+	   	// checking if user is admin of course on page, if so, show the edit capabilities, otherwise, no
+	   	for(var i=0; i<course.admins.length; i++){
+	   		var admin = course.admins[i];
+	   		if(admin._id===userId){
+	   			console.log('user is admin of course');
+	   			$scope.userIsAdminOfCourse = true;
+	   			break;
+	   		}
+	   		if(i===course.admins.length-1){
+	   			console.log('user is not admin of course!!');
+	   			$scope.userIsAdminOfCourse = false;
+	   		}
+	   	}
+	   	//_______________End of checking if user admin of course_______________
+
+
+	 });
+
+ }) // end of user checking login
 
 
 
-    $scope.goToTopic = function(topicId){
+	$scope.goToTopic = function (id) {
+		$state.go('topic', { topicId: id });
+	}
+
+
+
+
+
+
+	$scope.goToTopic = function(topicId){
         // console.log(courseId);
         // console.log(topicId);
         $state.go('topic', {courseId: courseId, topicId: topicId});
-    }
+      }
 
     //this is where the form for creating a topic will be controlled
     
     $scope.showing = false;
     
     $scope.showForm = function () {
-        $scope.showing = true;
+    	$scope.showing = true;
     }
     
     $scope.createTopic = function () {
-        $scope.showing = false;
-        courseService.postTopic($scope.newTopic, $stateParams.courseId).then(function(topic){
-            $scope.currentCourse.topics.push(topic);
-        });
-    }
+    	$scope.showing = false;
+    	courseService.postTopic($scope.newTopic, $stateParams.courseId).then(function(topic){
+    		$scope.course.topics.push(topic);
+    	});
+    };
 
 
 
-});
+
+
+  });
