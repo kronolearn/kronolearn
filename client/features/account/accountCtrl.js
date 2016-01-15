28964722ffefa0dd0704977b83fc2eab274adb06
Peter Krieg <pkrieg2@gmail.com>
2016-01-15 13:59:17 -0700

    added ability to change/add user picture, front to back.  used slightly different imageCtrl with AWS

--- client/features/account/accountCtrl.js
+++ client/features/account/accountCtrl.js
@@ -1,10 +1,30 @@
 angular.module('kronolearn')
-.controller('accountCtrl', function($scope, user){
+.controller('accountCtrl', function($scope, user, userService, $window){
 	console.log(user);
 
 	// if user object exists, attach to scope, otherwise user not logged in
 	if(user.name){
 		$scope.user = user;
+	}
+
+
+
+
+
+
+	$scope.addPicture = function(){
+		$scope.pictureAdded = true;
+	}
+
+	$scope.saveAccount = function(){
+		// console.log($scope.imageObj);
+
+		userService.saveUserAvatar($scope.imageObj, $scope.user)
+		.then(function(response){
+			// $window.location.reload();
+		});
+
+
 	}
 
 
