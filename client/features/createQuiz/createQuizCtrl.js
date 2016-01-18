6eaf2c4d3d43f091621db8bbb81db117d3a2ef56
SGMartineau <winteralce@gmail.com>
2016-01-18 11:07:34 -0700

    added a back button on the create cards view that takes you back to the topic page

--- client/features/createQuiz/createQuizCtrl.js
+++ client/features/createQuiz/createQuizCtrl.js
@@ -25,4 +25,8 @@ angular.module('kronolearn').controller('createQuizCtrl', function($scope, creat
         });
     }
     
+    $scope.goBack = function() {
+        window.history.back();
+    }
+    
 });
