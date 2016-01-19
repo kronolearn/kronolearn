689eafe3f65a454fee8b5e05d09d4cb5c105a3fe
Kyle <kyle.lauritzen@gmail.com>
2016-01-19 12:17:03 -0700

    on dashboard load, coursesToReview are calculated, and all cardsToReview are retrieved from database.

--- client/features/dashboard/dashboardCtrl.js
+++ client/features/dashboard/dashboardCtrl.js
@@ -7,22 +7,52 @@ app.controller('dashboardCtrl', function ($scope, user, $state, dashboardService
     // console.log($scope.user);
 
     
+    dashboardService.getUserAndCourses()
+        .then(function (user) {
+            $scope.userInfo = user;
+            $scope.courses = user.coursesEnrolledIn;
+            $scope.adminCourses = user.coursesAdminFor;
+            $scope.cards = user.cards;
+            //$scope.notify();
+       
+
     
     $scope.getCardsToReview = function() {
         
         dashboardService.getCardsToReview().then(function(response){
             console.log("Cards to review, dashboardCtrl data: ", response);
             $scope.cardsToReview = response;
+            $scope.getCoursesToReview();
+            console.log("Course IDs to review", $scope.courseIDsToReview);
+            console.log("Courses to review", $scope.coursesToReview);
         });
     }
     
     $scope.getCardsToReview();
     
+    $scope.courseIDsToReview = [];
+    
+    $scope.coursesToReview = [];
+    
+    $scope.getCoursesToReview = function() {
+        for (var i = 0; i < $scope.cardsToReview.length; i++) {
+            if ($scope.courseIDsToReview.indexOf($scope.cardsToReview[i].card.course) === -1) {
+                $scope.courseIDsToReview.push($scope.cardsToReview[i].card.course);
+            }
+        }
+        console.log("what is $scope.courses?: ", $scope.courses);
+        for (var j = 0; j < $scope.courseIDsToReview.length; j++) {
+            for (var k = 0; k < $scope.courses.length; k++) {
+                if (($scope.courses[k]._id === $scope.courseIDsToReview[j]/* && ($scope.coursesToReview.indexOf($scope.courses[k]) === -1)*/) && ($scope.coursesToReview.length < $scope.courseIDsToReview.length)) {
+                    $scope.coursesToReview.push($scope.courses[k]);
+                }
+            }
+        }
+    };
     
     
     
-    
-
+/*
     $scope.notify = function () {
         $scope.cards.forEach(function (card) {
             $scope.card = card;
@@ -36,16 +66,7 @@ app.controller('dashboardCtrl', function ($scope, user, $state, dashboardService
             console.log("no notifications today");
         }
 
-    };
-
-    dashboardService.getUserAndCourses()
-        .then(function (user) {
-            $scope.userInfo = user;
-            $scope.courses = user.coursesEnrolledIn;
-            $scope.adminCourses = user.coursesAdminFor;
-            $scope.cards = user.cards;
-            $scope.notify();
-        });
+    }; */
 
     $scope.topicClick = function (id) {
         $state.go('topic', { topicId: id });
@@ -62,4 +83,5 @@ app.controller('dashboardCtrl', function ($scope, user, $state, dashboardService
     $scope.addCourse = function () {
         $state.go('createCourse');
     };
+    });
 });
